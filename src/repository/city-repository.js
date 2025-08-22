97e66ea187ac181afdc88e4334d1e8dd07c0cb7c
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-08-22 16:32:57 +0530

    added the filteration

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,4 @@
-const { where } = require("sequelize");
+const { Op, where } = require("sequelize");
 const { City } = require("../models/index");
 
 class CityRepository {
@@ -63,8 +63,19 @@ class CityRepository {
     }
   }
 
-  async getAllCities() {
+  async getAllCities(filter) {
+    //if filter is empty ->we will be returing all the cites
     try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
       const cities = await City.findAll();
       return cities;
     } catch (error) {
